4e8da5f572d07cd2af2281359280d1fede68dc49
EmmanuelC1 <emcastillo@csumb.edu>
2023-06-29 19:24:02 -0700

    makes Star components more reusable by adding props as component"API"

--- src/components/starRating/StarRating.jsx
+++ src/components/starRating/StarRating.jsx
@@ -11,21 +11,32 @@ const starContainerStyle = {
   display: 'flex',
 };
 
-const textStyle = {
-  lineHeight: '1',
-  margin: '0',
-};
-
-export default function StarRating({ maxRating = 5 }) {
-  const [rating, setRating] = useState(0);
+export default function StarRating({
+  maxRating = 5,
+  color = '#fcc419',
+  size = 48,
+  className = '',
+  messages = [],
+  defaultRating = 0,
+  onSetRating,
+}) {
+  const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
 
   const handleRating = rating => {
     setRating(rating);
+    onSetRating(rating);
+  };
+
+  const textStyle = {
+    lineHeight: '1',
+    margin: '0',
+    color,
+    fontSize: `${size / 1.5}px`,
   };
 
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle} className={className}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
@@ -34,10 +45,16 @@ export default function StarRating({ maxRating = 5 }) {
             full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
             onHoverIn={() => setTempRating(i + 1)}
             onHoverOut={() => setTempRating(0)}
+            color={color}
+            size={size}
           />
         ))}
       </div>
-      <p style={textStyle}>{tempRating || rating || ''}</p>
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ''}
+      </p>
     </div>
   );
 }
