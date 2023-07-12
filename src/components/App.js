f4cd08894dbb2bf2f3c90871b633512494ce7f53
EmmanuelC1 <emcastillo@csumb.edu>
2023-07-11 17:20:44 -0700

    adds Loading component when fetching data

--- src/components/App.js
+++ src/components/App.js
@@ -10,20 +10,24 @@ import Box from './Box.jsx';
 import MovieList from './main/movieResults/MovieList.jsx';
 import Summary from './main/watchedMovies/Summary.jsx';
 import WatchedMovieList from './main/watchedMovies/WatchedMovieList.jsx';
+import Loader from './Loader.jsx';
 
 const KEY = 'fe42d655';
 
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(function () {
     async function fetchMovies() {
+      setIsLoading(true);
       const res = await fetch(
         `http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`
       );
       const data = await res.json();
       setMovies(data.Search);
+      setIsLoading(false);
     }
     fetchMovies();
   }, []);
@@ -37,9 +41,7 @@ export default function App() {
       </NavBar>
 
       <Main>
-        <Box>
-          <MovieList movies={movies} />
-        </Box>
+        <Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
 
         <Box>
           <Summary watched={watched} />
