dab4fbde8c9b8e45c4557757066a35c43ce3c643
emmanuelc1 <emmanuelcastillo1@gmail.com>
2023-07-26 20:57:26 -0700

    custom hook for fetching movies from query named useMovies

--- src/components/App.js
+++ src/components/App.js
@@ -11,23 +11,24 @@ import WatchedMovieList from './main/watchedMovies/WatchedMovieList.jsx';
 import Loader from './Loader.jsx';
 import ErrorMessage from './ErrorMessage.jsx';
 import MovieDetails from './main/watchedMovies/MovieDetails.jsx';
-import API_KEY from '../config/config.js';
+import { useMovies } from '../hooks/useMovies.js';
 
 export default function App() {
   const [query, setQuery] = useState('');
-  const [movies, setMovies] = useState([]);
+  const [selectedId, setSelectedId] = useState(null);
+  const { movies, isLoading, error } = useMovies(query); // Custom Hook
   const [watched, setWatched] = useState(() =>
     JSON.parse(localStorage.getItem('watched'))
   );
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState('');
-  const [selectedId, setSelectedId] = useState(null);
 
   const handleSelectMovie = id => {
     setSelectedId(selectedId => (id === selectedId ? null : id));
   };
 
-  const handleCloseSelectedMovie = () => setSelectedId(null);
+  function handleCloseSelectedMovie() {
+    // function declaration needed for hoisting
+    setSelectedId(null);
+  }
 
   const handleAddWatched = movie => {
     setWatched(watched => [...watched, movie]);
@@ -44,53 +45,7 @@ export default function App() {
     [watched]
   );
 
-  useEffect(
-    function () {
-      const controller = new AbortController();
-
-      async function fetchMovies() {
-        try {
-          setIsLoading(true);
-          setError('');
-          const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`,
-            { signal: controller.signal }
-          );
-
-          if (!res.ok)
-            throw new Error('Something went wrong while fetching movies');
-
-          const data = await res.json();
-          if (data.Response === 'False') throw new Error('Movie not found');
-
-          setMovies(data.Search);
-          setError('');
-        } catch (err) {
-          if (err.name !== 'AbortError') {
-            setError(err.message);
-          }
-        } finally {
-          setIsLoading(false);
-        }
-      }
-
-      if (query.length < 3) {
-        setMovies([]);
-        setError('');
-        return;
-      }
-
-      handleCloseSelectedMovie();
-      fetchMovies();
-
-      // Cleanup Function
-      return function () {
-        // cancels fetch requests after user keeps typing to avoid making too many requests
-        controller.abort();
-      };
-    },
-    [query]
-  );
+  // Custom Hook
 
   return (
     <>
