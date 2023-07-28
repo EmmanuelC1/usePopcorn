aa3e04a95df0d076c5b7822333c2e8e78eee465b
emmanuelc1 <emmanuelcastillo1@gmail.com>
2023-07-27 20:53:01 -0700

    refactored useEffect to custom useKey hook instead

--- src/components/navbar/Search.jsx
+++ src/components/navbar/Search.jsx
@@ -1,27 +1,17 @@
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
+import { useKey } from '../../hooks/useKey';
 
 export default function Search({ query, setQuery }) {
   const inputEl = useRef(null);
 
-  useEffect(
-    function () {
-      function callback(e) {
-        // If the search bar is already active, return
-        if (document.activeElement === inputEl.current) return;
+  // Custom hook to have searhc bar into focus when 'enter' key is press
+  useKey('Enter', function () {
+    // If the search bar is already active, return
+    if (document.activeElement === inputEl.current) return;
 
-        if (e.code === 'Enter') {
-          inputEl.current.focus();
-          setQuery('');
-        }
-      }
-
-      document.addEventListener('keydown', callback);
-
-      // Cleanup function
-      return () => document.addEventListener('keydown', callback);
-    },
-    [setQuery]
-  );
+    inputEl.current.focus();
+    setQuery('');
+  });
 
   return (
     <input
